df77597b5a35a8de58b047a3dc4bb72f247b713f
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-12-17 18:35:06 -0300

    deploy heroku

--- src/index.js
+++ src/index.js
@@ -314,7 +314,6 @@ app.get('/payment', (request, response) => {
 
 
 
-app.listen(3000, () => console.log('Listen'));
+//app.listen(3000, () => console.log('Listen'));
 
-//app.listen(process.env.PORT, () => console.log('Listen'));
-
+app.listen(process.env.PORT, () => console.log('Listen'));
